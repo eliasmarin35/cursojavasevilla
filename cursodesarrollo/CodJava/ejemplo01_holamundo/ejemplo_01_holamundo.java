40095773e1208f08a74a1fe3c852749cab36b011
elias <jemarinfernandez@gmail.com>
2025-07-16 14:31:40 +0200

    hola

---
+++ cursodesarrollo/CodJava/ejemplo01_holamundo/ejemplo_01_holamundo.java
@@ -0,0 +1,11 @@
+package cursodesarrollo.CodJava.ejemplo01_holamundo;
+
+/* Clase -> nombre del archivo sin .java */
+public class ejemplo_01_holamundo {
+    //snippet main
+    public static void main(String[] args) {
+        //snipet sout
+        System.out.println("Hola Mundillo");        
+    }
+    
+}
